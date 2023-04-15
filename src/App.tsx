512c14c32640495fb6133fcda526d338e9476775
keito <maple10032@gmail.com>
2023-04-15 21:36:51 +0900

    first

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Container } from "@mui/material";
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Container>
+      <h1>fdfs</h1>
+    </Container>
+  );
+}
+
+export default App;
